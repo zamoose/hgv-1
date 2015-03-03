fa1365f40889749eab2628d2d46ca33f780f9c20
Doug Stewart <zamoose@gmail.com>
2015-03-03 01:55:59 -0500

    Intermediate (non-functioning) dynamic inventory check-in.

--- bin/inventory.php
+++ bin/inventory.php
@@ -14,8 +14,8 @@ $core_file = __DIR__ . "/../provisioning/domains.yml";
 $core_array = array();
 
 // Optional user-supplied YAML
-$inventory_file = __DIR__ . "/../hgv_data/customdomains.yml";
-$inventory_array = array();
+$custom_file = __DIR__ . "/../hgv_data/customdomains.yml";
+$custom_array = array();
 
 //
 // Check to make sure nothing's gone wrong,
@@ -27,35 +27,55 @@ if( file_exists( $core_file ) ) {
     $core_inventory = array();
 
     foreach( $core_yaml as $group => $values ) {
-        $core_inventory[$group]["hosts"] = $values['hosts'];
-        $core_inventory[$group]["vars"] = $values['vars'];
+        $core_inventory[$group]['hosts'] = $values['hosts'];
+        $core_inventory[$group]['vars'] = $values['vars'];
     }
 
     // print_r( $core_inventory );
     //
+	echo "\n";
+	echo "Core Inventory";
     print_r(json_encode($core_inventory, JSON_PRETTY_PRINT ));
 } else {
     echo "ABORTING RUN. Core config file missing.";
 }
 
-// if( file_exists( $inventory_file ) ) {
-// 	$inventory = yaml_parse_file( $inventory_file );
-// 	if( !empty($options['host'] ) ) {
-// 		echo "Host: " . $options['host'];
-// 	}
-// 	foreach ( $inventory as $group => $values ) {
-// 		$hosts = array();
-// 		$ans_vars = array();
-// 		foreach ( $values as $host => $vars ){
-// 			$hosts[] = $host;
-// 			$ans_vars[] = $vars;
-// 		}
-// 		$inventory_array[$group] = array(
-// 			"hosts"	=> $hosts,
-// 			"vars"	=> $ans_vars
-// 		);
-// 	}
-// 	print_r(json_encode($inventory_array, JSON_PRETTY_PRINT ));
-// } else {
-// 	echo "It doesn't exist!\n";
-// }
+if( file_exists( $custom_file ) ) {
+	$custom_yaml = yaml_parse_file( $custom_file );
+	$custom_groups = array();
+	$custom_inventory = array();
+
+	foreach( $custom_yaml as $group => $values ) {
+		$custom_inventory[$group]['hosts'] = $values['hosts'];
+		$custom_inventory[$group]['vars'] = $values['vars'];
+	}
+	// if( !empty($options['host'] ) ) {
+	// 	echo "Host: " . $options['host'];
+	// }
+	// foreach ( $inventory as $group => $values ) {
+	// 	$hosts = array();
+	// 	$ans_vars = array();
+	// 	foreach ( $values as $host => $vars ){
+	// 		$hosts[] = $host;
+	// 		$ans_vars[] = $vars;
+	// 	}
+	// 	$inventory_array[$group] = array(
+	// 		"hosts"	=> $hosts,
+	// 		"vars"	=> $ans_vars
+	// 	);
+	// }
+	// print_r(json_encode($inventory_array, JSON_PRETTY_PRINT ));
+	echo "\n";
+	echo "Custom Inventory";
+	print_r(json_encode($custom_inventory, JSON_PRETTY_PRINT ));
+
+	$final_inventory = array_merge_recursive( $core_inventory, $custom_inventory );
+	$final_intersect = array_intersect( $core_inventory, $custom_inventory );
+
+	echo "\n";
+	echo "Merged Inventory";
+	print_r(json_encode($final_inventory, JSON_PRETTY_PRINT ));
+	echo "\n";
+	echo "Intersected Inventory";
+	print_r(json_encode($final_intersect, JSON_PRETTY_PRINT ));
+}
