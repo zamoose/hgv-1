6d4e2144a3ba052404d2229f7d6d6ff44446b59d
Doug Stewart <zamoose@gmail.com>
2015-04-23 16:30:00 -0400

    WPsites-related templating and tasks.

---
+++ provisioning/roles/wpsites/templates/wp/local-config.php
@@ -0,0 +1,42 @@
+<?php
+// ** MySQL settings ** //
+/** The name of the database for WordPress */
+define('DB_NAME', 'wpe_{{ item.site | regex_replace('\.','') }}');
+
+/** MySQL database username */
+define('DB_USER', 'wpe_{{ item.site | regex_replace('\.','') }}');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'wordpress');
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost');
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+define('WP_DEBUG', true);
+define('WP_DEBUG_DISPLAY', false);
+define('SCRIPT_DEBUG', true);
+define('SAVEQUERIES', true);
+
+global $memecached_servers;
+
+$memcached_servers = array(
+    'default' => array(
+        '127.0.0.1:11211'
+    )
+);
+
+$wp_cache_key_salt = 'wpe_{{ item.site | regex_replace('\.','') }}_1_';
+if( isset($_SERVER['HTTP_HOST']) ) {
+    define('WP_SITEURL', 'http://'.$_SERVER['HTTP_HOST']);
+    define('WP_HOME', 'http://'.$_SERVER['HTTP_HOST']);
+    $wp_cache_key_salt = 'wpe_{{ item.site | regex_replace('\.','') }}_'.$_SERVER['HTTP_HOST'].'_';
+}
+
+/** Object Cache Key Salt per domain */
+define('WP_CACHE_KEY_SALT', $wp_cache_key_salt);
